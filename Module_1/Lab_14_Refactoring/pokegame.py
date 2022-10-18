f941b25c46f4212c5e6f322d9986a5dc75d4c07a
psyrgkan <psyrganis@gmail.com>
2022-10-18 13:13:11 +0200

    some final comments

--- Module_1/Lab_14_Refactoring/pokegame.py
+++ Module_1/Lab_14_Refactoring/pokegame.py
@@ -1,7 +1,5 @@
-'''
-This is the main python file where the logic and practice for the fight simulator lives.
-
-'''
+"""This is the main python file where the logic and practice for the fight simulator lives.
+"""
 from attack import Attack
 from pokemon import Pokemon
 import pokeinfo
