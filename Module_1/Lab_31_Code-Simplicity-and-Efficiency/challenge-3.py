8c7a4fd76365353c5b8fff27d43c42aa68ba2eea
psyrgkan <psyrganis@gmail.com>
2022-10-26 15:46:48 +0200

    challenge 3 done in linear time

--- Module_1/Lab_31_Code-Simplicity-and-Efficiency/challenge-3.py
+++ Module_1/Lab_31_Code-Simplicity-and-Efficiency/challenge-3.py
@@ -11,20 +11,48 @@ For example, if you are given the number 15, there are 3 possibilities to compos
 The following function shows one way to solve the problem but the code is not ideal or efficient.
 Refactor the code based on what you have learned about code simplicity and efficiency.
 """
+import numpy as np
 
-def my_function(X):
-    solutions = []
-    for x in range(5, X):
-        for y in range(4, X):
-            for z in range(3, X):
-                if (x*x==y*y+z*z):
-                    solutions.append([x, y, z])
-    m = 0
-    for solution in solutions:
-        if m < max(solution):
-            m = max(solution)
-    return m
+x_input = int(input("What is the maximal length of the triangle side? Enter a number: "))
 
-X = input("What is the maximal length of the triangle side? Enter a number: ")
+def pythagoreanTriplets(x_input) :
+    c, m = 0, 2
+    triangles = []
+    
+    # Limiting c would limit
+    # all a, b and c
+    while c < x_input :
+         
+        # Now loop on n from 1 to m-1
+        for n in range(1, m) :
+            a = m**2 - n**2
+            b = 2 * m * n
+            c = m**2 + n**2
+ 
+            # if c is greater than
+            # limit then break it
+            if c > x_input :
+                break
+ 
+            print(a, b, c)
+            triangles.append([a, b, c])
 
-print("The longest side possible is " + str(my_function(int(X))))
+        m = m + 1
+
+    return np.amax(triangles)
+
+# This is ok but it is O(n^3), we can do it in O(k) if we use some math 
+# def my_function(X):
+#     solutions = []
+#     for x in range(5, X):
+#         for y in range(4, X):
+#             for z in range(3, X):
+#                 if (x*x==y*y+z*z):
+#                     solutions.append([x, y, z])
+#     m = 0
+#     for solution in solutions:
+#         if m < max(solution):
+#             m = max(solution)
+#     return m
+
+print("The longest side possible is %d" % pythagoreanTriplets(x_input))
